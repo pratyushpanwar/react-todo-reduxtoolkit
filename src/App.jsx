c880d320c576b5d51a2fab70a8ed75f1aa2800af
Pratyush Panwar <avipanwar1212@gmail.com>
2025-06-19 22:17:45 +0530

    add whats needed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <AddTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id:1, text: 'hello world'}]
+}
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload.text
+            }
+
+            state.todos.push(todo)
+        },
+
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+
+        updateTodo: (state, action) => {
+            state.todos = state.todos.map((todo)=> todo.id === action.payload
+            ? {...todo,text: action.payload.text } : todo)
+        },
+
+        
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
